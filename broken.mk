22797263c73deae366c50c264cf97bd50f77e114
ken greenwood <kengreenwood420666@gmail.com>
2015-02-05 09:37:57 -0600

    added boot ani size

--- broken.mk
+++ broken.mk
@@ -1,6 +1,10 @@
 # Release name
 PRODUCT_RELEASE_NAME := grouper
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 800
+
 # Inherit some common stuff.
 $(call inherit-product, vendor/broken/config/common_full_tablet_wifionly.mk)
 
